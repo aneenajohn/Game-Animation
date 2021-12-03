d1c535637ee90b62b3c966e1d9109ac34d2ad748
AllanSam <75213145+aneenajohn@users.noreply.github.com>
2021-12-04 02:12:30 +0530

    Added n number of objects in animated movement

--- script.js
+++ script.js
@@ -12,3 +12,64 @@ console.log(ctx);
 // To set up canvas, create custom global variables
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 700;
+
+//enemy1 = {
+//    x: 0,
+//    y: 0,
+//    width: 150,
+//    height: 150
+//}
+
+const numberOfEnemies = 100;
+const enemiesArray = [];
+class Enemy {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.a = Math.random() * canvas.width;
+        this.b = Math.random() * canvas.height;
+        this.width = 20;
+        this.height = 20;
+        this.speed = Math.random() * 4 - 2;
+    }
+    update() {
+        this.x += this.speed;
+        this.y += this.speed;
+        this.a += this.speed;
+        this.b += this.speed;
+    }
+    draw() {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.a, this.b, this.width, this.height)
+    }
+}
+
+//const enemy1 = new Enemy();
+
+//function animate() {
+//    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // to clear the old paint
+//    enemy1.x++;
+//    enemy1.y++
+//        ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+//    requestAnimationFrame(animate); // creates endless animation loop
+//}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+    enemiesArray.push(new Enemy());
+}
+
+console.log(enemiesArray);
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.clearRect(500, 700, CANVAS_HEIGHT, CANVAS_WIDTH);
+    //enemy1.update();
+    //enemy1.draw();
+    enemiesArray.forEach((enemy) => {
+        enemy.update();
+        enemy.draw();
+    })
+    requestAnimationFrame(animate);
+}
+
+animate();
