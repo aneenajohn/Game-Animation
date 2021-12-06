ed9ac1650afea6710825a9aa52c44f041501cdf4
Aneena John <75213145+aneenajohn@users.noreply.github.com>
2021-12-06 10:41:59 +0530

    Added wiggling movement

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const ctx = canvas.getContext('2d');
 
 // To set up canvas, create custom global variables
 CANVAS_WIDTH = canvas.width = 500;
-CANVAS_HEIGHT = canvas.height = 700;
+CANVAS_HEIGHT = canvas.height = 500;
 
-const numberOfEnemies = 40;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 // const enemyImage = new Image();
 // enemyImage.src = '/assets/enemy1.png'
@@ -22,25 +22,25 @@ class Enemy {
     constructor() {
         this.image = new Image();
         this.image.src = '/assets/enemy1.png'
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
         this.a = Math.random() * canvas.width;
         this.b = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        // this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293 // width of single enemy sprite
         this.spriteHeight = 155
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        // Make initial x nd y within the canvas
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         // animate sprites
         // gameFrame % 2 === 0 => all flap at same speed once after every animate func call
         if (gameFrame % this.flapSpeed === 0) {
-
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
